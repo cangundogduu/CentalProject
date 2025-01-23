4b6348fa5c72f72ae7a9d912e31627105e4ffa1b
can gündoğdu <gundogdu.can@hotmail.com>
2025-01-23 22:41:56 +0300

    23.01.2024

--- Cental.WebUI/Controllers/AdminCarController.cs
+++ Cental.WebUI/Controllers/AdminCarController.cs
@@ -1,15 +1,56 @@
-﻿using Cental.BusinessLayer.Abstract;
+﻿using AutoMapper;
+using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.CarDtos;
+using Cental.DtoLayer.Enums;
+using Cental.EntityLayer.Entities;
+using Cental.WebUI.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Cental.WebUI.Controllers
 {
-    public class AdminCarController(ICarService _carService) : Controller
+    public class AdminCarController(ICarService _carService,IMapper _mapper,IBrandService _brandService) : Controller
     {
         public IActionResult Index()
         {
             var values = _carService.TGetCarsWithBrands();
 
             return View(values);
+        }
+
+        public IActionResult CreateCar()
+        {
+            ViewBag.gasTypes = GetEnumValues.GetEnums<GasTypes>();
+            ViewBag.gearTypes = GetEnumValues.GetEnums<GearTypes>();
+            ViewBag.transmissionTypes = GetEnumValues.GetEnums<Transmissions>();
+            ViewBag.brands = (from x in _brandService.TGetAll()
+                              select new SelectListItem
+                              {
+                                  Text= x.BrandName,
+                                  Value= x.BrandId.ToString()
+                              }).ToList();
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCar(CreateCarDto model)
+        {
+            ViewBag.gasTypes = GetEnumValues.GetEnums<GasTypes>();
+            ViewBag.gearTypes = GetEnumValues.GetEnums<GearTypes>();
+            ViewBag.transmissionTypes = GetEnumValues.GetEnums<Transmissions>();
+            ViewBag.brands = (from x in _brandService.TGetAll()
+                              select new SelectListItem
+                              {
+                                  Text = x.BrandName,
+                                  Value = x.BrandId.ToString()
+                              }).ToList();
+
+
+            var newCar= _mapper.Map<Car>(model);
+            _carService.TCreate(newCar);
+            return RedirectToAction("Index");
         }
     }
 }
